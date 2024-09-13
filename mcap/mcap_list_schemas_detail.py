d030f204bc73e2573068945e03a0e8042833923a
MrB-Can <paul@bigeye.com>
2024-09-13 08:43:11 -0700

    Add truncate to long metadata to keep clean output When the metadata was long we were ouputting all of it which could take up pages of space and make the result unreadable. Truncated to 60.

--- mcap/mcap_list_schemas_detail.py
+++ mcap/mcap_list_schemas_detail.py
@@ -1,3 +1,4 @@
+import json
 import sys
 from mcap.reader import make_reader
 
@@ -22,14 +23,17 @@ def list_schemas_with_messages_and_metadata(mcap_file: str) -> None:
                 message_counts[schema_id] += 1
 
         # Display the schema information along with message count and metadata
-        print(f"{'Schema ID':<12}{'Name':<35}{'Encoding':<12}{'Message Count':<15}{'Metadata'}")
-        print('-' * 80)
+        print('\n')
+        print(f"{'Schema ID':<12}{'Name':<60}{'Encoding':<12}{'Message Count':<15}{'Metadata':<60}")
+        print('-' * 160)
         for schema_id in sorted(summary.schemas):
             schema = summary.schemas[schema_id]
             channel = summary.channels.get(schema_id, {})
             metadata = channel.metadata if channel else {}
-            metadata_str = ", ".join([f"{k}: {v}" for k, v in metadata.items()]) if metadata else "None"
-            print(f"{schema_id:<12}{schema.name:<35}{schema.encoding:<12}{message_counts[schema_id]:<15}{metadata_str}")
+            metadata_str = json.dumps(metadata)
+            if len(metadata_str) > 60:
+                metadata_str = metadata_str[:57] + " ..."
+            print(f"{schema_id:<12}{schema.name:<60}{schema.encoding:<12}{message_counts[schema_id]:<15}{metadata_str}")
 
 
 if __name__ == "__main__":
